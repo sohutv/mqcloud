1a134a3e4b4dae179af4c5cb442547e4f3e56630
yongfeigao <yongfeigao@MININT-ON89A7R.sohu-inc.com>
2020-11-11 14:45:22 +0800

    fix消息重发bug;移除测试错误配置;

--- mq-cloud/src/test/java/com/sohu/tv/mq/cloud/task/BrokerStoreStatTaskTest.java
+++ mq-cloud/src/test/java/com/sohu/tv/mq/cloud/task/BrokerStoreStatTaskTest.java
@@ -4,14 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.sohu.tv.mq.cloud.Application;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
-@Configuration
 public class BrokerStoreStatTaskTest {
     
     @Autowired
